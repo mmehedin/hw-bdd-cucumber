8e01aaf3d284433ebf5ad11ff754a105f9d3bd61
Mihai Mehedint <mmehedin@uncc.edu>
2016-03-29 15:55:45 +0000

    Some of part 2 subheading 1 and 2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -22,14 +22,41 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
+Given /I checked the movies only of rating 'PG' or 'R'/ do
+  check('ratings_PG')
+  check('ratings_R')
+  uncheck('ratings_PG-13')
+  uncheck('ratings_G')
+end
+
+When /I click on sumbit/ do
+  click_button('ratings_submit')
+end
+
+Then /I should see only 'PG' or 'R' rated movies/ do
+ page.body.should match(/<td>R<\/td>/)
+ page.body.should match(/<td>PG<\/td>/)
+end
+
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split(%r{,\s*})
+  if uncheck
+    ratings.each do |current_rate|
+      step "I uncheck \"#{current_rate}\""
+    end
+  else
+    ratings.each do |current_rate|
+      step "I check \"#{current_rate}\""
+    end
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
+  Movie.find(:all).length.should page.body.scan(/<tr>/).length
   fail "Unimplemented"
 end
